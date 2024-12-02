f4d845894251afccd8289f2dc81b45e65043bc40
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2024-12-02 15:01:31 -0300

    Conclusão da lógica

--- binary/main.js
+++ binary/main.js
@@ -1,12 +1,13 @@
-// Seleciona todos os elementos com a classe 'bloco'
 const blocos = document.querySelectorAll('.bloco');
+const resultado = document.querySelector('.resultado')
 
 // Função para alternar o estado do bloco
 function alternarBloco(event) {
     const bloco = event.target;
+    let isLit = bloco.classList.contains('bloco-aceso');
 
     // Alterna entre as classes 'bloco-aceso' e 'bloco-apagado'
-    if (bloco.classList.contains('bloco-aceso')) {
+    if (isLit) {
         bloco.classList.remove('bloco-aceso');
         bloco.classList.add('bloco-apagado');
         bloco.textContent = '0'; // Atualiza o texto do bloco
@@ -15,9 +16,29 @@ function alternarBloco(event) {
         bloco.classList.add('bloco-aceso');
         bloco.textContent = '1'; // Atualiza o texto do bloco
     }
+
+    somarResultado()
 }
 
 // Adiciona o evento de clique a todos os blocos
 blocos.forEach(bloco => {
     bloco.addEventListener('click', alternarBloco);
 });
+
+function somarResultado(){
+    const valores = [];
+    let binario = 1
+    let soma = 0;
+
+    blocos.forEach(bloco => {
+        valores.push(bloco.textContent);
+    });
+
+    valores.slice().reverse().forEach(numero => {
+        soma += binario * numero;
+
+        binario *= 2
+    });
+
+    resultado.innerHTML = `O resultado é: ${soma}`
+}
